3f6e8b669997ac66cd23c88cf8a98989ef0245df
Tomasz Cielecki <tomasz@ostebaronen.dk>
2018-07-05 23:28:50 +0200

    Add UWP version

---
+++ MavenRepoBrowser.UWP/DownloadService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+
+[assembly: Xamarin.Forms.Dependency(typeof(MavenRepoBrowser.UWP.DownloadService))]
+namespace MavenRepoBrowser.UWP
+{
+    public class DownloadService : IDownloadService
+    {
+        public DownloadService()
+        {
+        }
+
+        public async Task SaveStreamAsync(string filename, Stream stream)
+        {
+            var savePicker = new FileSavePicker
+            {
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+                SuggestedFileName = filename
+            };
+
+            var extension = Path.GetExtension(filename);
+            savePicker.FileTypeChoices.Add(extension, new List<string>() { extension });
+
+            StorageFile file = await savePicker.PickSaveFileAsync();
+            if (file != null)
+            {
+                CachedFileManager.DeferUpdates(file);
+                using (var fileStream = await file.OpenAsync(FileAccessMode.ReadWrite))
+                using (var outputStream = fileStream.GetOutputStreamAt(0))
+                using (var writeStream = outputStream.AsStreamForWrite())
+                {
+                    await stream.CopyToAsync(writeStream);
+                }
+            }
+        }
+    }
+}
